af0abe3c22b816ec023fa31ac1193f424065f524
Thulasimurugan <71541921+Thulasimurugan@users.noreply.github.com>
2025-04-07 11:45:25 +0530

    Delete Full Stack Project/Frontend directory

--- Full Stack Project/Frontend/Component/PrivateRoutes/index.js
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import Auth from "../../Auth";
-
-function PrivateRoutes() {
-    return Auth.isAuthenticatedUser() ?  <Outlet /> : <Navigate to='/'/>;
-};
-export default PrivateRoutes;
